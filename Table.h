ca1d3d4a8d602650388dd5ab08190b01e977c032
teologs <58266075+teologs@users.noreply.github.com>
2019-11-30 08:36:39 +0200

    Update Table.h

--- Table.h
+++ Table.h
@@ -1,7 +1,3 @@
-//
-// Created by Teodor Iliev on 2019-11-15.
-//
-
 #ifndef HUFFMAN_ALGORITHM_TABLE_H
 #define HUFFMAN_ALGORITHM_TABLE_H
 
@@ -15,18 +11,37 @@ class Table {
 private:
     int numberOfSavedSymbols;
     pair<int, char> array[256];
+    pair<string, char> binaryArray[256];
 public:
     Table();
     Table(string input);
+
     //void addNumberByIndex(int n) const;
     //void addNumberBySymbol(char a) const;
 
     int getNumberByIndex(int n) const;
     char getSymbolByIndex(int n) const;
     int getNumberBySymbol(char a) const;
+    void fillTableFromString(string input);
+    int getNumberOfSavedSymbols() const;
+    void setSymbolByIndex(int index, char symbol);
+    void setNumberByIndex(int index, string number);
+    void addNumberAndSymbol(string number, char symbol);
+
 
     bool checkIfSaved(char a);
     void addOneCount(char a);
+
+    void printTable(int n);
+    ///----------------------------
+
+    void setBinaryElement(string& binary, char symbol, int index);
+    void printBinaryTable(int n);
+    void setBinaryElementBySymbol(string& binary, char symbol);
+    void copySymbolsIntoBinaryArray();
+    string getBinaryNumberBySymbol(char symbol);
+    string getBinaryNumberByIndex(int index);
+    char getBinarySymbolByIndex(int index);
 
 
 
